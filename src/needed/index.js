a579eb8cb4ded1a4c49e461796f5f6b6bfd8ebb9
Josedpv <mobilefl44@gmail.com>
2021-02-07 23:42:01 -0400

    first commit

--- src/needed/index.js
+++ src/needed/index.js
@@ -129,7 +129,9 @@ function init()
 	var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 2, FAR = 10000;
 	camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
     
-	
+		
+	camera.position
+	camera.
 	
 	//Lights
 	// spotLight = new THREE.SpotLight( 0xffff00 );
@@ -143,7 +145,7 @@ function init()
 function addLights() 
 {
 	
-	
+	//Hemisphere light
 	scene.add( hemisLight );
 	spotLight = new THREE.SpotLight();
     spotLight.angle = Math.PI / 16;
@@ -152,35 +154,78 @@ function addLights()
     spotLight.position.set( obj.posX, obj.posY, obj.posZ );
 	scene.add( spotLight );
 	spotLightHelper = new THREE.SpotLightHelper( spotLight );
-	
+	//scene.add( spotLightHelper );
+	//fireworklight
+	/*var light = new THREE.PointLight(0xffffff);
+	light.position.set(-10000,0,10000);
+	scene.add(light);*/
 }
 
 function addGUI() 
 {
-	stats.showPanel( 0 ); 
+	stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
 	document.body.appendChild( stats.dom );
+	/*var guiALL= gui.addFolder('Light');
+	var guiSL = guiALL.addFolder('SpotLight');
+	guiSL.add(obj, 'helpSpot').onChange(function (val) {
+		spotLightHelper.visible = val;
+	});
+	guiSL.add(obj, 'posX').onChange(function (val) {
+		spotLight.position.x = val;
+		spotLightHelper.update();
+	});
+	guiSL.add(obj, 'posY').onChange(function (val) {
+		spotLight.position.y = val;
+		spotLightHelper.update();
 
+	});
+	guiSL.add(obj, 'posZ').onChange(function (val) {
+		spotLight.position.z = val;
+		spotLightHelper.update();
+
+	});
+	//Ambient Light
+	var guiAL = guiALL.addFolder('AmbientLight');
+	guiAL.addColor(obj, 'color0').onChange(function (val) {
+		light.color.set(val);
+		hemisLight.color.set(val);
+	});
+	guiAL.add(obj, 'intAmbien').min(0).max(1).step(0.1).onChange(function (val) {
+		light.intensity = val;
+	}).name('Intensity');
+
+	//Hemisphere Light
+	var guiHL = guiALL.addFolder('HemisphereLight');
+	guiHL.addColor(obj, 'colorg').onChange(function (val) {
+		hemisLight.groundColor.set(val);
+	});
+	guiHL.add(obj, 'intHemis').min(0).max(1).step(0.1).onChange(function (val) {
+		hemisLight.intensity = val;
+	}).name('Intensity');
+	
+*/
 	
 }
 
 function main() {
 
 	
-	
+	//Renderer
 	renderer.setClearColor(0x222222);
 	renderer.autoClearColor = false;
     renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.outputEncoding = THREE.sRGBEncoding;
-	
+	//renderer.gammaOutput = true;
     renderer.gammaFactor = 2.2;
-  
+   // renderer.shadowMap.enabled = true;
+//	renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
 	
 	//Camera
 	camera.position.x = 3000;
 	camera.position.y = 3000;
 	camera.position.z = 3000;
-	
+	//camera.lookAt( 0, 3000, 0 );
 	
     controls = new OrbitControls( camera, renderer.domElement );
 
@@ -230,9 +275,14 @@ function main() {
 	
 
 	addGUI();
-
+	//Sumarry= gui.addFolder('Index Currency');
+	
      
+//Glass sphere creator receive the radius of the sphere
 
+//Audio function and proposal for audio manager
+
+//space that rotate in sinusoidal wave
 const rotationSpace = new THREE.Object3D();
 
 
@@ -249,7 +299,7 @@ function addGUIChooseDate (){
 	var parameters=[] ;
 	var Date_elected=[];
 	var counter=0;
-	
+	//for (let indexx = 0; indexx < Date_number; indexx++) {}
 		for (let index = 0; index < info.length; index++) {
 			
 			if(info[index][0]=="Date:"){
@@ -364,7 +414,22 @@ function movement(direction, speed){
   	}
   	
 }
-
+function roundRect(ctx, x, y, w, h, r) 
+{
+    ctx.beginPath();
+    ctx.moveTo(x+r, y);
+    ctx.lineTo(x+w-r, y);
+    ctx.quadraticCurveTo(x+w, y, x+w, y+r);
+    ctx.lineTo(x+w, y+h-r);
+    ctx.quadraticCurveTo(x+w, y+h, x+w-r, y+h);
+    ctx.lineTo(x+r, y+h);
+    ctx.quadraticCurveTo(x, y+h, x, y+h-r);
+    ctx.lineTo(x, y+r);
+    ctx.quadraticCurveTo(x, y, x+r, y);
+    ctx.closePath();
+    ctx.fill();
+	ctx.stroke();   
+}
 function makeTextSprite( message)
 {	
 	var canvas = document.createElement('canvas');
@@ -372,12 +437,12 @@ function makeTextSprite( message)
 	canvas.height = 256;
 	var ctx = canvas.getContext("2d");
 
-	
+	//ctx.translate(100,0);
 	ctx.font = "24pt Arial";
 	ctx.fillStyle = "white";
 	ctx.textAlign = "center";
 	ctx.fillText( message, 128, 44);
-	
+	//console.log(ctx);
 	var tex = new THREE.Texture(canvas);
 	tex.needsUpdate = true;
 	var spriteMat = new THREE.SpriteMaterial({
@@ -389,7 +454,10 @@ function makeTextSprite( message)
 }
 
 function createwrittensphere(sphere_price, sphere_size,sphere_cant, colors,index){
-	
+	//Ground
+
+
+	 
 
 		var geom = new THREE.SphereGeometry(sphere_size, 34, 24);
 		var mat = new THREE.MeshPhongMaterial({
@@ -398,7 +466,7 @@ function createwrittensphere(sphere_price, sphere_size,sphere_cant, colors,index
     	  metalness: 1,
 		  emissiveIntensity:1,
 
-		  
+		  //wireframe: true
 		});
 		var planet = new THREE.Mesh(geom, mat);
 		var orbit = 1500;
@@ -408,7 +476,7 @@ function createwrittensphere(sphere_price, sphere_size,sphere_cant, colors,index
 		planet_z=planet.position.z;
 		planet_y=sphere_size;
 		planet_x=planet.position.x;
-		timestamp++;
+	timestamp++;
 		planet.userData.orbit = orbit;
 		planet.userData.speed = sphere_size;
 	
@@ -419,20 +487,20 @@ function createwrittensphere(sphere_price, sphere_size,sphere_cant, colors,index
 		canvas.height = 256;
 		var ctx = canvas.getContext("2d");
 		
-		
+		//ctx.translate(100,0);
 		ctx.font = "38pt Arial";
 		ctx.fillStyle = "white";
 		ctx.textAlign = "center";
 		ctx.fillText( sphere_price, 128, 44);
-		
+		//console.log(ctx);
 		var tex = new THREE.Texture(canvas);
 		tex.needsUpdate = true;
 		var spriteMat = new THREE.SpriteMaterial({
 		  map: tex
 		});
 		sprite_2[index] = new THREE.Sprite(spriteMat);
-		sprite_2[index].position.x =planet.position.x;
-		sprite_2[index].position.z = planet.position.z;
+		sprite_2[index].position.x =planet.position.x;// planet_x +(Math.cos(timestamp2 * 20) * orbit);
+		sprite_2[index].position.z = planet.position.z;//planet_z +( Math.sin(timestamp2 * 20) * orbit);
 		var scaleFactor = 9;
 		var scale = scaleVector.subVectors(planet.position, camera.position).length() / scaleFactor;
 		sprite_2[index].scale.set(scale, scale, 1);
@@ -440,17 +508,153 @@ function createwrittensphere(sphere_price, sphere_size,sphere_cant, colors,index
 		scene.add(sprite_2[index]);
 
 
-
+	//var sprite = makeTextSprite( sphere_cant );
+//	spritey.position.set(55,105,55);
+	//scene.add( spritey );
+	//planet.add(sprite);
 	var spritee = makeTextSprite(  sphere_cant);
 	spritee.visible=false;
 		planet.add(spritee);
 		planets.push(planet);
 		scene.add(planet);
 	
+		//orbit
+	
+	  
+	
+
+	/*const lettersTilt = new THREE.Object3D();
+	//scene.add(lettersTilt);
+	//lettersTilt.rotation.set(THREE.Math.degToRad(-15),0,THREE.Math.degToRad(-15));
+	const lettersBase = new THREE.Object3D();
+	lettersTilt.add(lettersBase);
+	/*{
+	const letterMaterial = new THREE.MeshPhongMaterial({
+		color: 'red',
+	});  
+	const loader = new THREE.FontLoader();
+	loader.load('https://threejsfundamentals.org/threejs/resources/threejs/fonts/helvetiker_regular.typeface.json', (font) => {
+		const spaceSize = 0.1;
+		let totalWidth = 0;
+		let maxHeight = 0;
+		const letterGeometries = {
+		' ': { width: spaceSize, height: 0 }, // prepopulate space ' '
+		};
+		const size = new THREE.Vector3();
+		//crear switch con sphere_number para escoger que precio e info lleva sphere_number
+			const str = sphere_price;//************************************************************************Informacion escrita********************************************* 
+
+			const letterInfos = str.split('').map((letter, ndx) => {
+		if (!letterGeometries[letter]) {
+			const geometry = new THREE.TextBufferGeometry(letter, {
+			font: font,
+			size: 6,
+			height: 0.4,
+			curveSegments: 24,
+			bevelEnabled: true,
+			bevelThickness: 1,
+			bevelSize: 0.6,
+			bevelSegments: 10,
+			});
+			geometry.computeBoundingBox();
+			geometry.boundingBox.getSize(size);
+			letterGeometries[letter] = {
+			geometry,
+			width: size.x / 2, // no idea why size.x is double size
+			height: size.y,
+			};
+		}
+		const {geometry, width, height} = letterGeometries[letter];
+		const mesh = geometry
+			? new THREE.Mesh(geometry, letterMaterial)
+			: null;
+		totalWidth += width;
+		maxHeight = Math.max(maxHeight, height);
+		
+		return {
+			mesh,
+			width,
+		};
+		});
+		let t = 0;
+		const radius = (sphere_size*4) / Math.PI;
+		for (const {mesh, width} of letterInfos) {
+		if (mesh) {
+			const offset = new THREE.Object3D();
+			lettersBase.add(offset);
+			//mesh.position.set(sphere_x,sphere_y,sphere_z);
+			
+			var orbit = 200;
+			offset.rotation.y = t / totalWidth * Math.PI * 2;
+			mesh.position.z = radius;
+			offset.add(mesh);
+			mesh.position.y =-maxHeight / 2;
+			offset.position.y =2.5;
+			offset.position.x = Math.cos(timestamp * sphere_size) * orbit;
+			offset.position.z = Math.sin(timestamp * sphere_size) * orbit;
+		}
+		t += width;
+		}
+		{
+		
+		
+		//************************************************************************posicion de la bola********************************************* 
+
+		
+		var spritey = makeTextSprite( sphere_price , 
+			{ fontsize: 32, fontface: "Georgia", borderColor: {r:0, g:0, b:255, a:1.0} } );
+		//spritey.position.setY(0.5);
+		
+		spritey.visible=false;
+		group.add(lettersTilt);
+		
+	
+		
+		}
+	
+		//camera.position.z = radius * 3;
+	});
+	}
+		
+	const geo = new THREE.SphereBufferGeometry(sphere_size, 32, 24);
+	const mat = new THREE.MeshPhongMaterial({
+	color: 'cyan',
+	});
+	//**********sphere_size**************************************************************tamano de la bola********************************************* 
+	const mesh = new THREE.Mesh(geo, mat);
+	var orbit = 200;
+	
+	mesh.position.x = Math.cos(timestamp * sphere_size) * orbit;
+	mesh.position.z = Math.sin(timestamp * sphere_size) * orbit;
+	timestamp++;
+
+
+	var canvas = document.createElement('canvas');
+    canvas.width = 256;
+    canvas.height = 256;
+    var ctx = canvas.getContext("2d");
+    ctx.font = "44pt Arial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText(sphere_price, 128, 44);
+    //console.log(ctx);
+    var tex = new THREE.Texture(canvas);
+    tex.needsUpdate = true;
+    var spriteMat = new THREE.SpriteMaterial({
+      map: tex
+    });
+    var sprite = new THREE.Sprite(spriteMat);
+
+	mesh.add(sprite);
+	
+		group.push( mesh );
+	
+
+		scene.add(group);*/
 }
 
 function createwrittensphere2( sphere_price, sphere_size, orbit, mat,sphere_name,indice){
-	
+	//Ground
 
 
 	 
@@ -474,12 +678,12 @@ function createwrittensphere2( sphere_price, sphere_size, orbit, mat,sphere_name
 		canvas.height = 256;
 		var ctx = canvas.getContext("2d");
 		
-		
+		//ctx.translate(100,0);
 		ctx.font = "18pt Arial";
 		ctx.fillStyle = "white";
 		ctx.textAlign = "center";
 		ctx.fillText(sphere_name, 128, 44);
-		
+		//console.log(ctx);
 		var tex = new THREE.Texture(canvas);
 		tex.needsUpdate = true;
 		var spriteMat = new THREE.SpriteMaterial({
@@ -488,8 +692,8 @@ function createwrittensphere2( sphere_price, sphere_size, orbit, mat,sphere_name
 		
 		sprite[indice] = new THREE.Sprite(spriteMat);
 		sprite[indice].visible=true;
-		sprite[indice].position.x =planet.position.x;
-		sprite[indice].position.z = planet.position.z;
+		sprite[indice].position.x =planet.position.x;// planet_x +(Math.cos(timestamp2 * 20) * orbit);
+		sprite[indice].position.z = planet.position.z;//planet_z +( Math.sin(timestamp2 * 20) * orbit);
 		
 		var scaleFactor = 9;
 		var scale = scaleVector.subVectors(planet.position, camera.position).length() / scaleFactor;
@@ -516,16 +720,31 @@ function raycast() {
   
 		  if ( intersects.length > 0 ) {
 			INTERSECTED = intersects[ 0 ].object;
-			
+			//alert(INTERSECTED.children[0]);
 			spriteee= INTERSECTED.children[0];
 			spriteee.visible=true;
+			//intersects.sprite.visible=true;
+			/*INTERSECTED.forEach( function(planet){
+				
+				alert(planet);
+				planet.sprite.visible=true;
+				
+				
+				
+			  });*/
+  
+			 // if ( INTERSECTED != intersects[ 0 ].object ) {
 	
+				 
+			
 				
 			  
 	
 		  } else {
 
-		
+			
+			//INTERSECTED = intersects[ 0 ].object;
+			//INTERSECTED.spritey.visible=true;
 			if (spriteee!=0) {
 				spriteee.visible=false;
 			}
@@ -544,7 +763,8 @@ function onMouseMove( event ) {
 }
 
 function exchange_sphere(where_to_start, register_number){
-;
+	//alert(where_to_start);
+	//alert(sphere_cant_exchange[register_number]);
 	
 	timestamp=0;
 	radii=0;
@@ -564,7 +784,7 @@ function exchange_sphere(where_to_start, register_number){
 		var actual=0;
 		var biggest_hijo=0;
 		var actual_hijo=0;
-		
+		//Sumarry= gui.addFolder('Index Currency');
 		for (let jndex=1 ; jndex < info[0].length-1; jndex++){
 			mat[jndex] = new THREE.MeshPhongMaterial({
 				emissive:  colors_array_2[jndex],
@@ -572,17 +792,23 @@ function exchange_sphere(where_to_start, register_number){
 				roughness: 0,
     	  metalness: 1,
 		  emissiveIntensity:1,
-				
+				//wireframe: true
 			});
-			
+			//Sumarry.addColor( mat[jndex], 'color'  ).name(info[nombres][jndex]);
 		}
 	
 	for (let indexx=0 ; indexx < sphere_cant_exchange[register_number]-4; indexx++) {
-		
+		//alert(info[index][sphere_cant_exchange[register_number]]);
+		//alert(info[index][0]);
 			var counter=0;
 			var first;
 				var regex=/,/gi;
-			
+				//console.log(info[index][0]);
+				//console.log(info[index][sphere_cant_exchange[register_number]]);
+				//alert(info[index][0]);
+				//alert(info[index][sphere_cant_exchange[register_number]]);
+				//info[index][0]
+				//info[index][sphere_cant_exchange[register_number]]
 				timestamp2=0;
 						counter++;
 						first= counter;
@@ -590,7 +816,7 @@ function exchange_sphere(where_to_start, register_number){
 						var $='$';
 						regex = $ ;
 						sphere_size= sphere_size.replace(regex, '') ;
-						
+						//alert(sphere_size);
 						for (let indexxx=1 ; indexxx < 12; indexxx++) {
 		
 							sphere_size=sphere_size/10;
@@ -603,6 +829,15 @@ function exchange_sphere(where_to_start, register_number){
 					
 						}
 					
+						
+						
+
+					//alert(sphere_size);
+						//info[index][0]=info[index][0]+'-------------------------';
+						//alert(info[index][0]);
+						//timestamp=sphere_size;
+						//alert(sphere_size+info[index][0]);
+						
 						
 						
 						createwrittensphere( info[index][0], sphere_size, info[index][sphere_cant_exchange[register_number]],colors_array_1[indexx],indexx);
@@ -630,7 +865,26 @@ function exchange_sphere(where_to_start, register_number){
 						var $='$';
 						regex = $ ;
 						sphere_size= sphere_size.replace(regex, '') ;
-				
+					//	alert(sphere_size);
+						/*for (let indexxx=1 ; indexxx < 12; indexxx++) {
+		
+							sphere_size=sphere_size/10;
+							
+							if (sphere_size<10){
+							
+								sphere_size=(indexxx-1)*20;
+								//alert(sphere_size);
+								indexxx=12;
+							}
+					
+						}*/
+
+						//alert(sphere_size);
+						
+						//console.log(info[nombres][jndex]);
+						//console.log(info[index][jndex]);
+						//alert(info[nombres][jndex]);
+						//alert(info[index][jndex]);
 						
 						createwrittensphere2( info[index][jndex], sphere_size/200000, 400,mat[jndex],info[nombres][jndex],number);
 						
@@ -657,7 +911,7 @@ function Start_Sphere(where_to_start, register_number)
 	camera.position.x = 0;
 	camera.position.y = 5000;
 	camera.position.z = 0;
-	
+	//gui.removeFolder(Sumarry);
 	planets.forEach( function(planet){
     
 		scene.remove( planet);
@@ -681,7 +935,8 @@ function Start_Sphere(where_to_start, register_number)
 		if( sprite_2!= []){ scene.remove( sprite_2[kndex]);}
 	  }
 	 
-	
+	//alert(camera.position);
+	//scene.remove( spritey);
 	 planets = [];
 	exchange_sphere(where_to_start, register_number);
  	
@@ -707,6 +962,8 @@ function displayWindowSize(){
 
 // Attaching the event listener function to window's resize event
 window.addEventListener("resize", displayWindowSize);
+// document.addEventListener( 'keydown', onKeyDown, false );
+// document.addEventListener( 'keyup', onKeyUp, false );
 
 function animate() 
 {
@@ -717,7 +974,8 @@ function animate()
     
 	var scaleFactor = 9;
 	var sprite = planet.children[0];
-	
+	//alert(planet.posX);
+	//sprite.position.set(sprite.posX, sprite.posY, sprite.posZ)
 	var scale = scaleVector.subVectors(planet.position, camera.position).length() / scaleFactor;
 	sprite.scale.set(scale*2, scale*2, 1);
 	
@@ -730,7 +988,8 @@ function animate()
   controls.update();
   stats.update();
   var dt = clock.getDelta();
-  
+  	
+  //controls.update();
 }
 
 
@@ -739,7 +998,10 @@ function render()
 	renderer.domElement.addEventListener( 'mousemove', onMouseMove );
 
 	const delta = clock.getDelta();
-	
+	//Para la animacion
+	//Array.prototype.forEach.call(parent.children, child => 	console.log(child) });
+	//const hasControlUpdated = cameraControls.update(delta);
+
 	
 }
 
